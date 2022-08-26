f2de939b5dddcc4b575c0b05002a367a4ca481d3
faroh7979 <faroh797979@gmail.com>
2022-08-26 19:25:18 +0300

    Update snake_old_fashioned_basic_project.py

--- snake_old_fashioned_basic_project.py
+++ snake_old_fashioned_basic_project.py
@@ -2,6 +2,18 @@ import sys
 import time
 
 
+class MyColors:
+    pink = '\033[95m'
+    blue = '\033[94m'
+    sky_blue = '\033[96m'
+    ocean_green = '\033[92m'
+    yellow = '\033[93m'
+    red = '\033[91m'
+    white = '\033[0m'
+    bold_white = '\033[1m'
+    underline = '\033[4m'
+
+
 def snake_speed(text: str, slow_time: float):
     sys.stdout.write(text)
     sys.stdout.flush()
@@ -15,34 +27,23 @@ def snake_field():
 
     for snake_seal in range(length):
         if snake_seal == 0:
-            print(' ', end='')
+            print(f"{MyColors.sky_blue + '|'}", end='')
         elif snake_seal == length - 1:
-            print('_')
+            print(f"{MyColors.sky_blue + '‾'}" f"{MyColors.sky_blue + '|'}")
         else:
-            print('_', end='')
+            print(f"{MyColors.sky_blue + '‾'}", end='')
 
     for columns in range(width):
         print('|', ' ' * (length - 3), '|')
 
     for snake_floor in range(length):
         if snake_floor == 0:
-            print(' ', end='')
+            print(f"{MyColors.sky_blue + ' '}", end='')
         elif snake_floor == length - 1:
-            print('_')
+            print(f"{MyColors.sky_blue + '‾'}", end='')
         else:
-            print('_', end='')
-
-
-class MyColors:
-    pink = '\033[95m'
-    blue = '\033[94m'
-    sky_blue = '\033[96m'
-    ocean_green = '\033[92m'
-    yellow = '\033[93m'
-    red = '\033[91m'
-    white = '\033[0m'
-    bold_white = '\033[1m'
-    underline = '\033[4m'
+            print(f"{MyColors.sky_blue + '‾'}", end='')
+    print()
 
 
 snake_field()
