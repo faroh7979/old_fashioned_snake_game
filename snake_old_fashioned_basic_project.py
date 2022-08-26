d3b0d094879afa48ec7a67d5fff3a5583fa9de14
faroh7979 <faroh797979@gmail.com>
2022-08-26 18:20:40 +0300

    Update snake_old_fashioned_basic_project.py

--- snake_old_fashioned_basic_project.py
+++ snake_old_fashioned_basic_project.py
@@ -1,1 +1,11 @@
-print("Hello SoftUni")
+import sys
+import time
+
+
+def snake_speed(text: str, slow_time: float):
+    sys.stdout.write(text)
+    sys.stdout.flush()
+    time.sleep(slow_time)
+
+
+snake_speed(f"* \n", 1)
