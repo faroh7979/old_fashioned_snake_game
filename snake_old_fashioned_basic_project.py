00cc6efdcc4f2fa6aa7034a39e32adffe6b26692
faroh7979 <faroh797979@gmail.com>
2022-08-26 18:40:55 +0300

    Update snake_old_fashioned_basic_project.py

--- snake_old_fashioned_basic_project.py
+++ snake_old_fashioned_basic_project.py
@@ -8,4 +8,16 @@ def snake_speed(text: str, slow_time: float):
     time.sleep(slow_time)
 
 
-snake_speed(f"* \n", 1)
+class MyColors:
+    pink = '\033[95m'
+    blue = '\033[94m'
+    sky_blue = '\033[96m'
+    ocean_green = '\033[92m'
+    yellow = '\033[93m'
+    red = '\033[91m'
+    white = '\033[0m'
+    bold_white = '\033[1m'
+    underline = '\033[4m'
+
+
+snake_speed(f"{MyColors.red + '*'} \n", 1)
